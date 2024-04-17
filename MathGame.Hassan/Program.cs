d153591e6f6d3bed88ed346193d9c06a1498c23f
Hassan Yahya <hassanyahya400@gmail.com>
2024-04-17 15:34:23 +0100

    refactor all

--- MathGame.Hassan/Program.cs
+++ MathGame.Hassan/Program.cs
@@ -1,97 +1,247 @@
 ﻿using System.Linq.Expressions;
+using static System.Formats.Asn1.AsnWriter;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello there! welcome to a math game");
+        Console.Write("Please enter your name:  ");
 
-        bool isActive = true;
-        bool error = false;
-        int output = 0;
-        int convertedFirstNum = 0;
-        int convertedSecondNum = 0;
-        //List<> history = new List<T>();
+        var gameHistory = new List<string>();
+        var date = DateTime.UtcNow;
+        var name = Console.ReadLine();
 
-        while (isActive)
+        Menu(name);
+
+        void Menu(string? name)
         {
-            Console.WriteLine("What operation would you love to perform?");
-            Console.WriteLine("\nA.'+'\nB.'-'\nC.'x'\nD.'÷'\n ");
-            string? operation = Console.ReadLine();
+            bool isGameOn = true;
+            printDivider();
+            Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek} today 😃. Welcome to the maths game!\n");
 
-            Console.Write("Enter your first number: ");
-            string? firstNum = Console.ReadLine();
-
-            Console.Write("Enter your second number: ");
-            string? secondNum = Console.ReadLine();
-
-            try
+            do
             {
-                convertedFirstNum = Convert.ToInt32(firstNum);
-                convertedSecondNum = Convert.ToInt32(secondNum);
-            }
-            catch
-            {
-                Console.WriteLine("Invalid first or second number, first num and second num must be an integer");
-                Console.WriteLine($"firstNum: {firstNum}, secondNum; {secondNum}");
-                error = true;
-            }
+                Console.Clear();
+                Console.WriteLine("What operation would you like to do?\n" +
+                                   "V - View history " +
+                                   "A - Addition " +
+                                   "S - Subtration " +
+                                   "D - Division " +
+                                   "M - Multiplication " +
+                                   "Q - Quit game "
+               );
 
-            //try
-            //{
-            //    operation = operation.ToUpper();
-            //}
-            //catch
-            //{
-            //    Console.WriteLine("Invalid operation supplied, operation must be one of the items in the list");
-            //    Console.WriteLine($"operation: {operation}");
-            //    error = true;
-            //}
+                printDivider();
 
-            //if (operation != "A" || operation != "B" || operation != "C" || operation != "D")
-            //{
-            //    Console.WriteLine("Invalid operation supplied, operation must be one of the items in the list");
-            //    Console.WriteLine($"operation: {operation}");
-            //    error = true;
-            //}
+                var operationSelected = Console.ReadLine();
 
-            if (!error)
-            {
-                switch (operation?.ToUpper())
+                switch (operationSelected.Trim().ToUpper())
                 {
+                    case "V":
+                        GetHistory();
+                        break;
                     case "A":
-                        output = convertedFirstNum + convertedSecondNum;
+                        AdditionOperation();
                         break;
-                    case "B":
-                        output = convertedFirstNum - convertedSecondNum;
-                        break;
-                    case "C":
-                        output = convertedFirstNum * convertedSecondNum;
+                    case "S":
+                        SubtractionOperation();
                         break;
                     case "D":
-                        output = convertedFirstNum / convertedSecondNum;
+                        DivisionOperation();
+                        break;
+                    case "M":
+                        MultiplicationOperation();
+                        break;
+                    case "Q":
+                        Console.WriteLine("GOODBYE 👋 ");
+                        isGameOn = false;
                         break;
                     default:
-                        Console.WriteLine("Invalid operation passed, please select one of A. B. C. D.");
+                        Console.WriteLine("Invalid operation selected ❌");
+                        Environment.Exit(1);
                         break;
                 }
-            }
 
-            Console.Write($"ANSWER: {output}\n");
+            } while (isGameOn);
+        }
 
-            Console.WriteLine($"Would you love to continue? Enter 'Y' for yes or 'N' for no: ");
-            string? continueCalculation = Console.ReadLine();
+        void MultiplicationOperation()
+        {
+            int score = 0;
+            var random = new Random();
 
-            if (continueCalculation == "Y" || continueCalculation == "y")
+            for (int i = 1; i <= 5; ++i)
             {
-                isActive = true;
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} x {secondNumber}");
+                var answer = Console.ReadLine();
+
+                if (int.Parse(answer) == firstNumber * secondNumber)
+                {
+                    score++;
+                    Console.WriteLine("Congratulations! Your answer is correct. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ooops! Your answer is wrong. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+
             }
-            else
+
+            AddHistory(score, "Multiplication");
+
+            Console.WriteLine($"Your total score is {score}pts.\nGame over. Press any key to go back to main menu");
+            Console.ReadLine();
+        }
+
+        void DivisionOperation()
+        {
+            int score = 0;
+
+            for (int i = 1; i <= 5; ++i)
             {
-                isActive = false;
+                int[] divisionNumbers = GetDivisionNumbers();
+                int firstNumber = divisionNumbers[0];
+                int secondNumber = divisionNumbers[1];
+
+                Console.WriteLine($"{firstNumber} ÷ {secondNumber}");
+                var answer = Console.ReadLine();
+
+                if (int.Parse(answer) == firstNumber/secondNumber)
+                {
+                    score++;
+                    Console.WriteLine("Congratulations! Your answer is correct. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ooops! Your answer is wrong. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+
             }
+
+            AddHistory(score, "Division");
+
+            Console.WriteLine($"Your total score is {score}pts.\nGame over. Press any key to go back to main menu");
+            Console.ReadLine();
+        }
+
+        void SubtractionOperation()
+        {
+            int score = 0;
+            var random = new Random();
+
+            for (int i = 1; i <= 5; ++i)
+            {
+                int firstNumber = random.Next(0, 9);
+                int secondNumber = random.Next(0, 9);
+
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                var answer = Console.ReadLine();
+
+                if (int.Parse(answer) == firstNumber - secondNumber)
+                {
+                    score++;
+                    Console.WriteLine("Congratulations! Your answer is correct. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ooops! Your answer is wrong. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+
+            }
+
+            AddHistory(score, "Subtraction");
+
+            Console.WriteLine($"Your total score is {score}pts.\nGame over. Press any key to go back to main menu");
+            Console.ReadLine();
+        }
+
+        void AdditionOperation()
+        {
+            int score = 0;
+            var random = new Random();
+
+            for (int i = 1; i <=5; ++i)
+            {
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                var answer = Console.ReadLine();
+
+                if (int.Parse(answer) == firstNumber + secondNumber)
+                {
+                    score++;
+                    Console.WriteLine("Congratulations! Your answer is correct. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ooops! Your answer is wrong. Press any key to go see the next question");
+                    Console.ReadLine();
+                }
+
+            }
+
+            AddHistory(score, "Addition");
+
+            Console.WriteLine($"Your total score is {score}pts.\nGame over. Press any key to go back to main menu");
+            Console.ReadLine();
+        }
+
+        int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+            int[] numbers = new int[2];
+            int firstNumber = random.Next(1, 99);
+            int secondNumber = random.Next(1, 99);
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            numbers[0] = firstNumber;
+            numbers[1] = secondNumber;
+
+            return numbers;
+        }
+
+        static void printDivider()
+        {
+            Console.WriteLine("|--------------------------------------------------------------------------|");
+        }
+
+        void GetHistory()
+        {
+            Console.Clear();
+            printDivider();
+
+            foreach (string game in gameHistory)
+            {
+                Console.WriteLine(game);
+            }
+
+            printDivider();
+            Console.WriteLine($"Press any key to go back to main menu");
+            Console.ReadLine();
 
         }
-        Console.WriteLine("Exiting application...");
+
+        void AddHistory(int gameScore, string gameType)
+        {
+            gameHistory.Add($"{date} | {gameType} | score: {gameScore}pts");
+        }
+
+
     }
 }
